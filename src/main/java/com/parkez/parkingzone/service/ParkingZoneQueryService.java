9ee352e632d97af866e76ea9add19ecf065c517d
codingTrip <codingtrip@naver.com>
2025-04-04 16:01:21 +0900

    fix: 테스트 커버리지 설정 수정

--- src/main/java/com/parkez/parkingzone/service/ParkingZoneQueryService.java
+++ src/main/java/com/parkez/parkingzone/service/ParkingZoneQueryService.java
@@ -1,9 +1,18 @@
 package com.parkez.parkingzone.service;
 
+import com.parkez.parkingzone.domain.repository.ParkingZoneRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional(readOnly = true)
+@RequiredArgsConstructor
 public class ParkingZoneQueryService {
+
+    private final ParkingZoneRepository parkingZoneRepository;
+
+    public void findParkingZone () {
+        parkingZoneRepository.findAll();
+    }
 }
