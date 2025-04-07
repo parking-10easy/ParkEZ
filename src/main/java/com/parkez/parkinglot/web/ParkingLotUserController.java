f22f2ece1321cc7c13f4bb44ad2bff60deec5d8c
Seoyeon <dev_seoyeon@mythologyteams.com>
2025-04-07 11:52:28 +0900

    feat: 컨트롤러에 swagger 추가 #12

--- src/main/java/com/parkez/parkinglot/web/ParkingLotUserController.java
+++ src/main/java/com/parkez/parkinglot/web/ParkingLotUserController.java
@@ -3,6 +3,7 @@ package com.parkez.parkinglot.web;
 import com.parkez.common.response.Response;
 import com.parkez.parkinglot.dto.response.ParkingLotSearchResponse;
 import com.parkez.parkinglot.service.ParkingLotService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
@@ -11,13 +12,14 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
-@Tag(name = "User용 주차장 조회 API", description = "Userr가 사용하는 가게 조회 API입니다.")
+@Tag(name = "User용 주차장 조회 API", description = "User가 사용하는 가게 조회 API입니다.")
 public class ParkingLotUserController {
 
     private final ParkingLotService parkingLotService;
 
     // 주차장 다건 조회
     @GetMapping("/v1/parking-lots")
+    @Operation(summary = "주차장 다건 조회")
     public Response<ParkingLotSearchResponse> searchParkingLots(
             @RequestParam(required = false) String name,
             @RequestParam(required = false) String address,
@@ -28,6 +30,7 @@ public class ParkingLotUserController {
 
     // 주차장 단건 조회
     @GetMapping("/v1/parking-lots/{parkingLotId}")
+    @Operation(summary = "주차장 단건 조회")
     public Response<ParkingLotSearchResponse> getParkingLot(
             @PathVariable Long parkingLotId
     ){
