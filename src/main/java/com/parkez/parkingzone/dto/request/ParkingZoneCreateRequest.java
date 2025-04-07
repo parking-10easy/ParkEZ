c60026e0b8119b07812b1a7aa638860edf3cc9e3
codingTrip <codingtrip@naver.com>
2025-04-07 13:53:31 +0900

    refactor: 주차 공간 생성 - DTO swagger 스키마 수정 #6

--- src/main/java/com/parkez/parkingzone/dto/request/ParkingZoneCreateRequest.java
+++ src/main/java/com/parkez/parkingzone/dto/request/ParkingZoneCreateRequest.java
@@ -1,5 +1,6 @@
 package com.parkez.parkingzone.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
@@ -8,16 +9,19 @@ import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@Schema(description = "주차공간 생성 요청")
 public class ParkingZoneCreateRequest {
 
     @NotNull(message = "주차장 아이디 값 입력은 필수입니다")
+    @Schema(description = "주차장 아이디", example = "1")
     private Long parkingLotId;
     @NotBlank(message = "이름 값 입력은 필수입니다")
+    @Schema(description = "주차공간명", example = "A구역")
     private String name;
     private String imageUrl;
 
     @Builder
-    private ParkingZoneCreateRequest(Long parkingLotId, String name, String imageUrl) {
+    private ParkingZoneCreateRequest(Long parkingLotId, String name) {
         this.parkingLotId = parkingLotId;
         this.name = name;
         this.imageUrl = imageUrl;
