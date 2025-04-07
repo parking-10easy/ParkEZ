1bdc0ff3e53892cd3b224c2053a3765a710ffef8
codingTrip <codingtrip@naver.com>
2025-04-07 14:01:22 +0900

    refactor: 주차 공간 수정 - DTO swagger 스키마 수정 #19

--- src/main/java/com/parkez/parkingzone/dto/request/ParkingZoneUpdateRequest.java
+++ src/main/java/com/parkez/parkingzone/dto/request/ParkingZoneUpdateRequest.java
@@ -1,13 +1,16 @@
 package com.parkez.parkingzone.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Schema(description = "주차공간 수정 요청")
 public class ParkingZoneUpdateRequest {
 
     @NotBlank(message = "이름 값 입력은 필수입니다")
+    @Schema(description = "주차공간명", example = "B구역")
     private String name;
 
     @Builder
