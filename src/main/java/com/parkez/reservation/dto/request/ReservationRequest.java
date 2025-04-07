06bfc5f6413417924ae7602c6f5712305b5adb25
JangYounhyuk <hyuk905@gmail.com>
2025-04-07 19:53:43 +0900

    refactor: reservation request schema 및 jsonformat 설정

--- src/main/java/com/parkez/reservation/dto/request/ReservationRequest.java
+++ src/main/java/com/parkez/reservation/dto/request/ReservationRequest.java
@@ -1,5 +1,7 @@
 package com.parkez.reservation.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
@@ -8,10 +10,17 @@ import java.time.LocalDateTime;
 @Getter
 public class ReservationRequest {
 
-    @NotNull
+    @NotNull(message = "주차공간 id는 필수값입니다.")
+    @Schema(description = "주차공간 id", example = "1")
     private Long parkingZoneId;
-    @NotNull
+
+    @NotNull(message = "시작 날짜 및 시간은 필수값입니다.")
+    @Schema(description = "시작 날짜 및 시간", example = "2025.04.07 10:00")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy.MM.dd HH:mm")
     private LocalDateTime startDateTime;
-    @NotNull
+
+    @NotNull(message = "종료 날짜 및 시간은 필수값입니다.")
+    @Schema(description = "종료 날짜 및 시간", example = "2025.04.07 10:00")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy.MM.dd HH:mm")
     private LocalDateTime endDateTime;
 }
