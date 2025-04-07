e4cc01c962a7d42e1864d21dd1ad2a2603e526d1
codingTrip <codingtrip@naver.com>
2025-04-07 14:02:42 +0900

    refactor: 주차 공간 상태 변경 - DTO swagger 스키마 수정 #23

--- src/main/java/com/parkez/parkingzone/dto/request/ParkingZoneUpdateStatusRequest.java
+++ src/main/java/com/parkez/parkingzone/dto/request/ParkingZoneUpdateStatusRequest.java
@@ -1,14 +1,17 @@
 package com.parkez.parkingzone.dto.request;
 
 import com.parkez.parkingzone.domain.enums.ParkingZoneStatus;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Schema(description = "주차공간 상태 변경 요청")
 public class ParkingZoneUpdateStatusRequest {
 
     @NotNull(message = "상태 값 입력은 필수입니다")
+    @Schema(description = "주차공간 상태", example = "UNAVAILABLE")
     private ParkingZoneStatus status;
 
     @Builder
