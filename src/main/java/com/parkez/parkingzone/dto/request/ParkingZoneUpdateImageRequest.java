d05a0a46cc308a7527572aee96107fc7f3fc932c
codingTrip <codingtrip@naver.com>
2025-04-07 13:59:35 +0900

    refactor: 주차 공간 이미지 수정 - DTO swagger 스키마 수정 #24

--- src/main/java/com/parkez/parkingzone/dto/request/ParkingZoneUpdateImageRequest.java
+++ src/main/java/com/parkez/parkingzone/dto/request/ParkingZoneUpdateImageRequest.java
@@ -1,13 +1,16 @@
 package com.parkez.parkingzone.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Schema(description = "주차공간 생성 요청")
 public class ParkingZoneUpdateImageRequest {
 
     @NotBlank(message = "이미지 값 입력은 필수입니다")
+    @Schema(description = "이미지 주소", example = "http://example.com/image.jpg")
     private String imageUrl;
 
     @Builder
