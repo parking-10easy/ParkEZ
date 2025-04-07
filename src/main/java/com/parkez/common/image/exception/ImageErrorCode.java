f58f3fa19f809394ab22238e2c5c63c05fe34282
minjonyyy <minjonyyy@naver.com>
2025-04-07 15:48:16 +0900

    feat : 이미지 CRUD 에러코드 추가 #8

---
+++ src/main/java/com/parkez/common/image/exception/ImageErrorCode.java
@@ -0,0 +1,21 @@
+package com.parkez.common.image.exception;
+
+import com.parkez.common.exception.ErrorCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum ImageErrorCode implements ErrorCode {
+
+    IMAGE_NOT_FOUND("IMAGE_NOT_FOUND", HttpStatus.NOT_FOUND, "해당 폴더에 이미지가 존재하지 않습니다."),
+    IMAGE_UPLOAD_FAIL("IMAGE_UPLOAD_FAIL", HttpStatus.INTERNAL_SERVER_ERROR , "서버 오류로 인해 이미지 업로드가 실패되었습니다."),
+    INVALID_IMAGE_TYPE("INVALID_IMAGE_TYPE",HttpStatus.BAD_REQUEST , "유효하지 않는 이미지 타입입니다.");
+
+    private final String code;
+    private final HttpStatus httpStatus;
+    private final String defaultMessage;
+
+
+}
