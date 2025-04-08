36edced1deb54abc9af1b0394cddf5014580380b
minjonyyy <minjonyyy@naver.com>
2025-04-08 10:54:08 +0900

    refactor : url 매핑 수정

--- src/main/java/com/parkez/common/image/ImageController.java
+++ src/main/java/com/parkez/common/image/ImageController.java
@@ -5,23 +5,20 @@ import com.parkez.common.image.dto.ImageUploadRequest;
 import com.parkez.common.response.Response;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/v1/images")
 @RequiredArgsConstructor
 @Tag(name = "이미지 업로드 및 삭제 API", description = "AWS S3와 연동된 .")
 public class ImageController {
 
-    private final ImageService imageService;
+    private final S3ImageService imageService;
 
-    @PostMapping("/v1/images")
+    @PostMapping
     public Response<List<String>> upload(
             @RequestPart("request") ImageUploadRequest request,
             @RequestPart("images") List<MultipartFile> imageFiles) {
@@ -30,7 +27,7 @@ public class ImageController {
         return Response.of(urls);
     }
 
-    @PutMapping("/v1/update")
+    @PutMapping
     public Response<List<String>> update(
             @RequestPart("request") ImageUploadRequest request,
             @RequestPart("images") List<MultipartFile> imageFiles
@@ -39,7 +36,7 @@ public class ImageController {
         return Response.of(updatedUrls);
     }
 
-    @DeleteMapping("/v1/delete-images")
+    @DeleteMapping
     public Response<Void> deleteImages(
             @RequestBody ImageDeleteRequest request
     ) {
