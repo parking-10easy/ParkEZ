1534bb9a571320e1c1a4a08005ff519d8595759a
minjonyyy <minjonyyy@naver.com>
2025-04-08 10:52:00 +0900

    feat : 에러코드 추가 #8

--- src/main/java/com/parkez/common/image/exception/ImageErrorCode.java
+++ src/main/java/com/parkez/common/image/exception/ImageErrorCode.java
@@ -11,7 +11,9 @@ public enum ImageErrorCode implements ErrorCode {
 
     IMAGE_NOT_FOUND("IMAGE_NOT_FOUND", HttpStatus.NOT_FOUND, "해당 폴더에 이미지가 존재하지 않습니다."),
     IMAGE_UPLOAD_FAIL("IMAGE_UPLOAD_FAIL", HttpStatus.INTERNAL_SERVER_ERROR , "서버 오류로 인해 이미지 업로드가 실패되었습니다."),
-    INVALID_IMAGE_TYPE("INVALID_IMAGE_TYPE",HttpStatus.BAD_REQUEST , "유효하지 않는 이미지 타입입니다.");
+    INVALID_IMAGE_TYPE("INVALID_IMAGE_TYPE",HttpStatus.BAD_REQUEST , "유효하지 않는 이미지 타입입니다."),
+    IMAGE_IS_NULL("IMAGE_IS_NULL", HttpStatus.BAD_REQUEST, "이미지 파일이 비어있습니다."),
+    INVALID_EXTENSION_TYPE("INVALID_EXTENSION_TYPE", HttpStatus.BAD_REQUEST, "허용되지 않는 파일 확장자입니다.");
 
     private final String code;
     private final HttpStatus httpStatus;
