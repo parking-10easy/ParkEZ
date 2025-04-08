1d5dd824459048a1292f040c16a2ff88695a2437
jun5-soodal <j01024749503@gmail.com>
2025-04-08 16:15:24 +0900

    style: 에러 코드 HttpStatus 기준으로 정렬 #39

--- src/main/java/com/parkez/auth/exception/AuthErrorCode.java
+++ src/main/java/com/parkez/auth/exception/AuthErrorCode.java
@@ -10,14 +10,20 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum AuthErrorCode implements ErrorCode {
-	//TODO Status 점검 필요
-	TOKEN_NOT_FOUND(HttpStatus.NOT_FOUND, "AUTH_001", "JWT 토큰을 찾을 수 없습니다."),
+
+	// 401 Unauthorized
 	INVALID_JWT_SIGNATURE(HttpStatus.UNAUTHORIZED, "AUTH_002", "유효하지 않은 JWT 서명입니다."),
 	TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, "AUTH_003", "만료된 JWT 토큰입니다."),
 	UNSUPPORTED_TOKEN(HttpStatus.UNAUTHORIZED, "AUTH_004", "지원하지 않는 JWT 토큰입니다."),
-	DUPLICATED_EMAIL(HttpStatus.CONFLICT, "AUTH_005", "이미 가입된 이메일입니다."),
 	INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "AUTH_006", "유효하지 않은 비밀번호 입니다."),
 	UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "AUTH_007", "로그인이 필요합니다."),
+
+	// 404 Not Found
+	TOKEN_NOT_FOUND(HttpStatus.NOT_FOUND, "AUTH_001", "JWT 토큰을 찾을 수 없습니다."),
+
+	// 409 Conflict
+	DUPLICATED_EMAIL(HttpStatus.CONFLICT, "AUTH_005", "이미 가입된 이메일입니다."),
+
 	;
 	private final HttpStatus httpStatus;
 	private final String code;
--- src/main/java/com/parkez/common/exception/CommonErrorCode.java
+++ src/main/java/com/parkez/common/exception/CommonErrorCode.java
@@ -9,9 +9,14 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum CommonErrorCode implements ErrorCode{
 
+	// 403 Forbidden
 	ACCESS_DENIED(HttpStatus.FORBIDDEN, "COMMON_001", "접근 권한이 없습니다."),
+
+	// 404 Not Found
+	AUTHORITY_NOT_FOUND(HttpStatus.NOT_FOUND, "COMMON_003", "권한이 없는 사용자입니다."),
+
+	// 500 Internal Server Error
 	INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "COMMON_002", "서버 오류가 발생했습니다."),
-	AUTHORITY_NOT_FOUND(HttpStatus.NOT_FOUND,"COMMON_003" , "권한이 없는 사용자입니다."),
 	;
 
 	private final HttpStatus httpStatus;
