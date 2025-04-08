78ec1adf2a77cd41eb1bfff0a04343f75bd17d20
minjonyyy <minjonyyy@naver.com>
2025-04-08 16:12:52 +0900

    add : 스웨거 @Operation 추가, refactor : 서비스 로직 인터페이스 의존으로 수정 #43

--- src/main/java/com/parkez/common/image/ImageController.java
+++ src/main/java/com/parkez/common/image/ImageController.java
@@ -3,6 +3,7 @@ package com.parkez.common.image;
 import com.parkez.common.image.dto.ImageDeleteRequest;
 import com.parkez.common.image.dto.ImageUploadRequest;
 import com.parkez.common.response.Response;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -13,12 +14,13 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/images")
 @RequiredArgsConstructor
-@Tag(name = "이미지 업로드 및 삭제 API", description = "AWS S3와 연동된 .")
+@Tag(name = "이미지 업로드 및 삭제 API", description = "AWS S3와 연동된 이미지 업로드/수정/삭제 기능입니다.")
 public class ImageController {
 
-    private final S3ImageService imageService;
+    private final ImageService imageService;
 
     @PostMapping
+    @Operation(summary = "이미지 업로드", description = "targetType과 targetId로 구분된 폴더에 s3 객체가 생성됩니다.")
     public Response<List<String>> upload(
             @RequestPart("request") ImageUploadRequest request,
             @RequestPart("images") List<MultipartFile> imageFiles) {
@@ -28,6 +30,7 @@ public class ImageController {
     }
 
     @PutMapping
+    @Operation(summary = "이미지 수정", description = "기존 객체를 지우고 새로운 객체로 업데이트합니다.")
     public Response<List<String>> update(
             @RequestPart("request") ImageUploadRequest request,
             @RequestPart("images") List<MultipartFile> imageFiles
@@ -37,6 +40,7 @@ public class ImageController {
     }
 
     @DeleteMapping
+    @Operation(summary = "이미지 삭제", description = "s3 객체를 삭제합니다.")
     public Response<Void> deleteImages(
             @RequestBody ImageDeleteRequest request
     ) {
