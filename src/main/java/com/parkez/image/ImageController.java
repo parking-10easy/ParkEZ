b32e586dcba13ba038cdaa7d81a7fe5d22bcf72b
minjonyyy <minjonyyy@naver.com>
2025-04-09 10:12:13 +0900

    refactor : request dto 하나로 통일  #43

--- src/main/java/com/parkez/image/ImageController.java
+++ src/main/java/com/parkez/image/ImageController.java
@@ -1,7 +1,6 @@
 package com.parkez.image;
 
-import com.parkez.image.dto.request.ImageDeleteRequest;
-import com.parkez.image.dto.request.ImageUploadRequest;
+import com.parkez.image.dto.request.ImageRequest;
 import com.parkez.image.dto.response.ImageUrlResponse;
 import com.parkez.common.response.Response;
 import io.swagger.v3.oas.annotations.Operation;
@@ -23,7 +22,7 @@ public class ImageController {
     @PostMapping
     @Operation(summary = "이미지 업로드", description = "targetType과 targetId로 구분된 폴더에 s3 객체가 생성됩니다.")
     public Response<ImageUrlResponse> upload(
-            @RequestPart("request") ImageUploadRequest request,
+            @RequestPart("request") ImageRequest request,
             @RequestPart("images") List<MultipartFile> imageFiles) {
 
         return Response.of(imageService.upload(request, imageFiles));
@@ -33,17 +32,17 @@ public class ImageController {
     @PutMapping
     @Operation(summary = "이미지 수정", description = "기존 객체를 지우고 새로운 객체로 업데이트합니다.")
     public Response<ImageUrlResponse> update(
-            @RequestPart("request") ImageUploadRequest request,
+            @RequestPart("request") ImageRequest request,
             @RequestPart("images") List<MultipartFile> imageFiles
     ){
         ;
-        return Response.of(imageService.update(request, imageFiles));
+        return Response.of(imageService.replace(request, imageFiles));
     }
 
     @DeleteMapping
     @Operation(summary = "이미지 삭제", description = "s3 객체를 삭제합니다.")
     public Response<Void> deleteImages(
-            @RequestBody ImageDeleteRequest request
+            @RequestBody ImageRequest request
     ) {
         imageService.delete(request);
         return Response.empty();
