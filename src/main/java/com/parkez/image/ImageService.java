91a589d6bbcb54a928ee09c529025bb8f798ec85
minjonyyy <minjonyyy@naver.com>
2025-04-09 10:12:37 +0900

    refactor : 메서드명 replace로 변경  #43

--- src/main/java/com/parkez/image/ImageService.java
+++ src/main/java/com/parkez/image/ImageService.java
@@ -1,7 +1,6 @@
 package com.parkez.image;
 
-import com.parkez.image.dto.request.ImageDeleteRequest;
-import com.parkez.image.dto.request.ImageUploadRequest;
+import com.parkez.image.dto.request.ImageRequest;
 import com.parkez.image.dto.response.ImageUrlResponse;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -9,7 +8,7 @@ import java.util.List;
 
 public interface ImageService {
 
-    ImageUrlResponse upload(ImageUploadRequest request, List<MultipartFile> files);
-    ImageUrlResponse update(ImageUploadRequest request, List<MultipartFile> files);
-    void delete(ImageDeleteRequest request);
+    ImageUrlResponse upload(ImageRequest request, List<MultipartFile> files);
+    ImageUrlResponse replace(ImageRequest request, List<MultipartFile> files);
+    void delete(ImageRequest request);
 }
