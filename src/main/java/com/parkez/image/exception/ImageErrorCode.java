4ddd1f129421a63025f8462c2597634f0eed7870
minjonyyy <minjonyyy@naver.com>
2025-04-09 10:32:25 +0900

    style : 에러코드 구분 #43

--- src/main/java/com/parkez/image/exception/ImageErrorCode.java
+++ src/main/java/com/parkez/image/exception/ImageErrorCode.java
@@ -9,8 +9,13 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum ImageErrorCode implements ErrorCode {
 
+    // 404 NOT FOUND
     IMAGE_NOT_FOUND(HttpStatus.NOT_FOUND, "IMAGE_001", "해당 폴더에 이미지가 존재하지 않습니다."),
+
+    // 500 internet server error
     IMAGE_UPLOAD_FAIL(HttpStatus.INTERNAL_SERVER_ERROR, "IMAGE_002", "서버 오류로 인해 이미지 업로드가 실패되었습니다."),
+
+    // 400 bad request
     INVALID_TARGET_TYPE(HttpStatus.BAD_REQUEST, "IMAGE_003", "유효하지 않는 타입입니다."),
     IMAGE_IS_NULL(HttpStatus.BAD_REQUEST, "IMAGE_004", "이미지 파일이 비어있습니다."),
     INVALID_EXTENSION_TYPE(HttpStatus.BAD_REQUEST, "IMAGE_005", "허용되지 않는 파일 확장자입니다.");
