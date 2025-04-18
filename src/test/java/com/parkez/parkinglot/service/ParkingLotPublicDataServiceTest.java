7211ca64d53c86d24ecf03838c801b23b7f84212
Seoyeon <dev_seoyeon@mythologyteams.com>
2025-04-18 16:18:16 +0900

    test(parking-lot): 공공데이터 테스트 수정 #79

--- src/test/java/com/parkez/parkinglot/service/ParkingLotPublicDataServiceTest.java
+++ src/test/java/com/parkez/parkinglot/service/ParkingLotPublicDataServiceTest.java
@@ -1,8 +1,8 @@
 package com.parkez.parkinglot.service;
 
+import com.parkez.parkinglot.client.kakaomap.geocode.KakaoGeocodeClient;
 import com.parkez.parkinglot.client.publicData.ParkingLotData;
 import com.parkez.parkinglot.client.publicData.ParkingLotDataResponse;
-import com.parkez.parkinglot.domain.entity.ParkingLot;
 import com.parkez.parkinglot.domain.repository.ParkingLotRepository;
 import com.parkez.user.domain.entity.User;
 import com.parkez.user.domain.enums.UserRole;
@@ -12,11 +12,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentCaptor;
-import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.core.BatchPreparedStatementSetter;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -24,11 +23,10 @@ import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.web.client.RestTemplate;
 
 import java.net.URI;
-import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.BDDMockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -47,54 +45,39 @@ public class ParkingLotPublicDataServiceTest {
     @Mock
     private JdbcTemplate jdbcTemplate;
 
+    @Mock
+    private KakaoGeocodeClient kakaoGeocodeClient;
+
     @InjectMocks
     private ParkingLotPublicDataService parkingLotPublicDataService;
 
     @BeforeEach
     public void setUp() throws IllegalAccessException {
+        ReflectionTestUtils.setField(parkingLotPublicDataService, "adminEmail", "admin@example.com");
         FieldUtils.writeField(parkingLotPublicDataService, "parkingLotPublicDataUrl", "http://dummy.url", true);
         FieldUtils.writeField(parkingLotPublicDataService, "serviceKey", "dummyServiceKey", true);
         FieldUtils.writeField(parkingLotPublicDataService, "defaultParkingLotImageUrl", "http://dummy.image.url", true);
         FieldUtils.writeField(parkingLotPublicDataService, "restTemplate", restTemplate, true);
     }
 
-    private ParkingLotData getNewParkingLotData() {
+    private ParkingLotData getParkingLotData() {
         return ParkingLotData.builder()
-                .name("새로운 공공데이터 주차장")
-                .address("새로운 주소")
-                .latitude("37.111")
-                .longitude("127.111")
-                .quantity("100")
+                .name("주차장 데이터")
+                .address("서울시 강남구")
+                .latitude("127.12345")
+                .longitude("37.98765")
+                .openedAt("09:00")
+                .closedAt("18:00")
+                .quantity("10")
+                .chargeType("무료")
                 .build();
     }
 
-    private ParkingLotData getExistingParkingLotData() {
-        return ParkingLotData.builder()
-                .name("존재하는 공공데이터 주차장")
-                .address("존재하는 주소")
-                .latitude("37.222")
-                .longitude("127.222")
-                .quantity("100")
-                .build();
-    }
-
-    private ParkingLot getExistngParkingLot() {
-        User user = getOwnerUser();
-        when(userReader.getActiveUserById(1L)).thenReturn(user);
-        ParkingLotData existingParkingLotData = getExistingParkingLotData();
-
-        return ParkingLot.builder()
-                .owner(user)
-                .name(existingParkingLotData.getName())
-                .address("저장된 주소")
-                .build();
-    }
-
-    private User getOwnerUser() {
+    private User getAdminUser() {
         User ownerUser = User.builder()
-                .email("owner@example.com")
-                .nickname("Owner")
-                .role(UserRole.ROLE_OWNER)
+                .email("admin@example.com")
+                .nickname("관리자")
+                .role(UserRole.ROLE_ADMIN)
                 .build();
         ReflectionTestUtils.setField(ownerUser, "id", 1L);
         return ownerUser;
@@ -103,20 +86,13 @@ public class ParkingLotPublicDataServiceTest {
     @Nested
     class fetchAndSavePublicData {
         @Test
-        void 주차장_공공데이터가_정상_저장된다() {
+        void 공공데이터가_정상_저장된다() {
 
             // given
-            ParkingLotData data1 = getNewParkingLotData();
-            ParkingLotData data2 = getExistingParkingLotData();
-            List<ParkingLotData> dataList = Arrays.asList(data1, data2);
+            ParkingLotData data = getParkingLotData();
 
             ParkingLotDataResponse dataResponse = ParkingLotDataResponse.builder()
-                    .page(1)
-                    .perPage(10)
-                    .totalCount(10)
-                    .currentCount(2)
-                    .matchCount(2)
-                    .data(dataList)
+                    .data(List.of(data))
                     .build();
 
             ResponseEntity<ParkingLotDataResponse> responseEntity = ResponseEntity.ok(dataResponse);
@@ -124,55 +100,56 @@ public class ParkingLotPublicDataServiceTest {
             when(restTemplate.getForEntity(any(URI.class), eq(ParkingLotDataResponse.class)))
                     .thenReturn(responseEntity);
 
+            when(kakaoGeocodeClient.getAddress(any(), any())).thenReturn(data.getAddress());
+
+            User adminUser = getAdminUser();
+            when(userReader.getUserByEmailAndRole(adminUser.getEmail(), adminUser.getRole()))
+                    .thenReturn(adminUser);
+
             //when
             parkingLotPublicDataService.fetchAndSavePublicData();
 
             //then
-            // API로 받은 데이터의 건수가 perPage(10)보다 작으므로 currentPage가 리셋
             assertEquals(1, parkingLotPublicDataService.getCurrentPage());
-
-            verify(jdbcTemplate, times(2))
-                    .batchUpdate(anyString(), ArgumentMatchers.<BatchPreparedStatementSetter>any());
-
+            verify(jdbcTemplate, times(2)).batchUpdate(anyString(), any(BatchPreparedStatementSetter.class));
         }
 
-//        @Test
-//        void 신규_주차장_공공데이터가_저장된다() {
-//            // given
-//            ParkingLotData newParkingLotData = getNewParkingLotData();
-//            List<ParkingLotData> dataList = List.of(newParkingLotData);
-//
-//            ParkingLotDataResponse dataResponse = ParkingLotDataResponse.builder()
-//                    .page(1)
-//                    .perPage(10)
-//                    .totalCount(1)
-//                    .currentCount(1)
-//                    .matchCount(1)
-//                    .data(dataList)
-//                    .build();
-//
-//            ResponseEntity<ParkingLotDataResponse> responseEntity = ResponseEntity.ok(dataResponse);
-//
-//            when(restTemplate.getForEntity(any(URI.class), eq(ParkingLotDataResponse.class)))
-//                    .thenReturn(responseEntity);
-//
-//            when(parkingLotRepository.findByNameIn(any())).thenReturn(Collections.emptyList());
-//
-//            // when
-//            parkingLotPublicDataService.fetchAndSavePublicData();
-//
-//            // then
-//            assertEquals(1, parkingLotPublicDataService.getCurrentPage());
-//            ArgumentCaptor<List<ParkingLot>> captor = ArgumentCaptor.forClass(List.class);
-//            verify(parkingLotRepository).saveAll(captor.capture());
-//            List<ParkingLot> savedList = captor.getValue();
-//            assertEquals(1, savedList.size());
-//            assertEquals(newParkingLotData.getName(), savedList.get(0).getName());
-//
-//        }
+        @Test
+        void DB에_있는_공공데이터는_저장되지_않는다() {
+            // given
+            ParkingLotData data = getParkingLotData();
+
+            ParkingLotDataResponse dataResponse = ParkingLotDataResponse.builder()
+                    .data(List.of(data))
+                    .build();
+
+            ResponseEntity<ParkingLotDataResponse> responseEntity = ResponseEntity.ok(dataResponse);
+
+            when(restTemplate.getForEntity(any(URI.class), eq(ParkingLotDataResponse.class)))
+                    .thenReturn(responseEntity);
+
+            when(kakaoGeocodeClient.getAddress(any(), any())).thenReturn(data.getAddress());
+
+            User adminUser = getAdminUser();
+            when(userReader.getUserByEmailAndRole(adminUser.getEmail(), adminUser.getRole()))
+                    .thenReturn(adminUser);
+
+            doThrow(DataIntegrityViolationException.class)
+                    .when(jdbcTemplate).batchUpdate(contains("INSERT INTO parking_lot"), any(BatchPreparedStatementSetter.class));
+
+            // when
+            parkingLotPublicDataService.fetchAndSavePublicData();
+
+            // then
+            verify(jdbcTemplate, times(1))
+                    .batchUpdate(contains("INSERT INTO parking_lot"), any(BatchPreparedStatementSetter.class));
+
+            verify(jdbcTemplate, never())
+                    .batchUpdate(contains("INSERT INTO parking_lot_image"), any(BatchPreparedStatementSetter.class));
+        }
 
         @Test
-        void 데이터_응답이_null_이면_currentPage가_리셋된다() {
+        void 데이터_응답이_null_이면_저장되지_않고_페이지가_초기화된다() {
             // given
             when(restTemplate.getForEntity(any(URI.class), eq(ParkingLotDataResponse.class)))
                     .thenReturn(ResponseEntity.ok(null));
@@ -207,36 +184,46 @@ public class ParkingLotPublicDataServiceTest {
 
         }
 
-//        @Test
-//        void 데이터의_수가_perPage보다_작으면_currentPage가_리셋된다(){
-//            // given
-//            ReflectionTestUtils.setField(parkingLotPublicDataService, "currentPage", 5);
-//
-//            ParkingLotData newData = getNewParkingLotData();
-//            List<ParkingLotData> dataList = List.of(newData);
-//            ParkingLotDataResponse dataResponse = ParkingLotDataResponse.builder()
-//                    .page(5)
-//                    .perPage(10)
-//                    .totalCount(1)
-//                    .currentCount(1)
-//                    .matchCount(1)
-//                    .data(dataList)
-//                    .build();
-//
-//            ResponseEntity<ParkingLotDataResponse> responseEntity = ResponseEntity.ok(dataResponse);
-//            when(restTemplate.getForEntity(any(URI.class), eq(ParkingLotDataResponse.class)))
-//                    .thenReturn(responseEntity);
-//
-//            ParkingLot existngParkingLot = getExistngParkingLot();
-//            when(parkingLotRepository.findByNameIn(any())).thenReturn(List.of(existngParkingLot));
-//
-//            // when
-//            parkingLotPublicDataService.fetchAndSavePublicData();
-//
-//            // then
-//            assertEquals(1, parkingLotPublicDataService.getCurrentPage());
-//        }
+        @Test
+        void 데이터의_수가_perPage보다_작으면_currentPage가_리셋된다() {
+            // given
+            ReflectionTestUtils.setField(parkingLotPublicDataService, "currentPage", 5);
+            ParkingLotData data = getParkingLotData();
+
+            List<ParkingLotData> dataList = List.of(data);
+            ParkingLotDataResponse dataResponse = ParkingLotDataResponse.builder()
+                    .data(dataList)
+                    .build();
+
+            ResponseEntity<ParkingLotDataResponse> responseEntity = ResponseEntity.ok(dataResponse);
+            when(restTemplate.getForEntity(any(URI.class), eq(ParkingLotDataResponse.class)))
+                    .thenReturn(responseEntity);
+
+            when(kakaoGeocodeClient.getAddress(any(), any())).thenReturn(data.getAddress());
+
+            User adminUser = getAdminUser();
+            when(userReader.getUserByEmailAndRole(adminUser.getEmail(), adminUser.getRole()))
+                    .thenReturn(adminUser);
+
+            when(jdbcTemplate.batchUpdate(anyString(), any(BatchPreparedStatementSetter.class)))
+                    .thenReturn(new int[]{1});
+            // when
+            parkingLotPublicDataService.fetchAndSavePublicData();
+
+            // then
+            assertEquals(1, parkingLotPublicDataService.getCurrentPage());
+        }
+
+
+        @Test
+        void API_호출_실패시_예외가_발생한다() {
+            // given
+            when(restTemplate.getForEntity(any(URI.class), eq(ParkingLotDataResponse.class)))
+                    .thenThrow(new RuntimeException("API 호출 실패"));
+
+            // when, then
+            assertThrows(RuntimeException.class, () -> parkingLotPublicDataService.fetchAndSavePublicData());
+        }
+
     }
-
-
 }
