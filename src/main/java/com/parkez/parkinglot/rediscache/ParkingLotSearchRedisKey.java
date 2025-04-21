452cdc7230c7aa94dce74c940f38267b4dcc72cb
JangYounhyuk <hyuk905@gmail.com>
2025-04-21 21:00:44 +0900

    refactor: refactoring

--- src/main/java/com/parkez/parkinglot/rediscache/ParkingLotSearchRedisKey.java
+++ src/main/java/com/parkez/parkinglot/rediscache/ParkingLotSearchRedisKey.java
@@ -7,8 +7,8 @@ public class ParkingLotSearchRedisKey {
                                           int page, int size
     ) {
         return String.format("parking-lots-name:%s-addr:%s-lat:%s-lng:%s-radius:%s-page:%d-size:%d",
-                name != null ? name : "null",
-                address != null ? address : "null",
+                name != null ? "val:" + name : "null",
+                address != null ? "val:" + address : "null",
                 userLatitude != null ? (int) (userLatitude * 1000) : "null", // 위도/경도는 0.001 단위로 라운딩 (약 100m 단위)
                 userLongitude != null ? (int) (userLongitude * 1000) : "null", // 위도/경도는 0.001 단위로 라운딩 (약 100m 단위)
                 radiusInMeters != null ? radiusInMeters : "null",
