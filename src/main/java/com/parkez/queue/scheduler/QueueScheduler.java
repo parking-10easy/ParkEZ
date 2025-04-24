baea90a20ff6061ef5b55425771dd911be188e22
minjonyyy <minjonyyy@naver.com>
2025-04-25 00:21:39 +0900

    feat : 스케줄러 락 추가 #120

--- src/main/java/com/parkez/queue/scheduler/QueueScheduler.java
+++ src/main/java/com/parkez/queue/scheduler/QueueScheduler.java
@@ -3,6 +3,7 @@ package com.parkez.queue.scheduler;
 import com.parkez.queue.service.QueueService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -14,9 +15,8 @@ public class QueueScheduler {
     private final QueueService queueService;
 
     @Scheduled(fixedDelay = 60_000) // 1분마다 실행
+    @SchedulerLock(name = "QueueScheduler_deleteQueues", lockAtLeastFor = "55s", lockAtMostFor = "2m")
     public void deleteQueues() {
         queueService.deleteExpiredQueues();
     }
-
-
 }
