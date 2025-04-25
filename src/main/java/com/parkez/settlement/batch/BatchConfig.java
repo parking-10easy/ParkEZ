f1ac9034c80580f4d438ff1bda32b0dfb0edc4bf
JangYounhyuk <hyuk905@gmail.com>
2025-04-25 16:14:14 +0900

    refactor: batch Job name, Step name 상수로 설정 #116

--- src/main/java/com/parkez/settlement/batch/BatchConfig.java
+++ src/main/java/com/parkez/settlement/batch/BatchConfig.java
@@ -27,13 +27,15 @@ import java.time.YearMonth;
 public class BatchConfig {
 
     private final SettlementService settlementService;
+    private static final String SETTLEMENT_JOB = "settlementJob";
+    private static final String SETTLEMENT_STEP = "settlementStep";
 
     @Bean
     // 첫번째 매개변수 : 해당 Job을 지칭할 이름 선언 ("settlementJob")
     // 두번째 매개변수 : 해당 작업에 대한 트래킹을 진행하기 위해 jobRepository를 넣어주면
     //                스프링 배치가 자동으로 작업이 진행되는지를 메타 데이터 테이블에 기록해준다.
     public Job settlementJob(JobRepository jobRepository, Step settlementStep) {
-        return new JobBuilder("settlementJob", jobRepository)
+        return new JobBuilder(SETTLEMENT_JOB, jobRepository)
                 .start(settlementStep) // 이 작업에서 처음 시작할 Step 선언, Step이 1개 이상일 때, next()로 이후의 Step을 선언
                 .build();
     }
@@ -47,7 +49,7 @@ public class BatchConfig {
             ItemWriter<SettlementBatchProcessResponse> settlementItemWriter
     ) {
         // <[Reader에서 읽어들일 데이터 타입], [Writer에서 쓸 데이터 타입]>
-        return new StepBuilder("settlementStep", jobRepository)
+        return new StepBuilder(SETTLEMENT_STEP, jobRepository)
                 .<User, SettlementBatchProcessResponse>chunk(10, platformTransactionManager) // chunkSize = 10 -> 10개 데이터 단위로 읽기, 처리, 쓰기 진행
                 .reader(ownerItemReader) // 읽는 메서드 자리
                 .processor(settlementItemProcessor) // 처리 메서드 자리
