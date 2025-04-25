dda2c3aeede05874b0791a07ee436433b6d50812
codingTrip <codingtrip@naver.com>
2025-04-25 11:52:57 +0900

    refactor: AlarmService title, content string format 사용 #114

--- src/main/java/com/parkez/alarm/service/AlarmService.java
+++ src/main/java/com/parkez/alarm/service/AlarmService.java
@@ -22,9 +22,12 @@ public class AlarmService {
         Long userId = reservation.getUserId();
         Long reservationId = reservation.getId();
 
-        String title = "[예약] " + reservation.getParkingZoneName() +
-                (notificationType == NotificationType.UPCOMING ? " 예약 종료 예정" : " 예약 만료");
-        String message = notificationType == NotificationType.UPCOMING ? "10분 후 예약이 종료됩니다." : "예약이 종료되었습니다.";
+        String title = String.format("[예약] %s %s",
+                reservation.getParkingZoneName(),
+                notificationType == NotificationType.UPCOMING ? "예약 종료 예정" : "예약 만료");
+
+        String message = String.format("%s",
+                notificationType == NotificationType.UPCOMING ? "10분 후 예약이 종료됩니다." : "예약이 종료되었습니다.");
 
         // 이메일 알림 생성
         boolean emailExists = alarmRepository.existsAlarm(reservationId, AlarmTargetType.RESERVATION, notificationType, AlarmChannel.EMAIL);
@@ -47,9 +50,12 @@ public class AlarmService {
         Long userId = reservationAlarmInfo.getUserId();
         Long reservationId = reservationAlarmInfo.getReservationId();
 
-        String title = "[결제] " + reservationAlarmInfo.getReservationId() +
-                (notificationType == NotificationType.CANCELED ? "번 예약 결제 취소" : "번 예약 결제 실패");
-        String message = notificationType == NotificationType.CANCELED ? "결제가 취소되었습니다." : "결제가 실패했습니다.";
+        String title = String.format("[결제] %d번 예약 결제 %s",
+                reservationAlarmInfo.getReservationId(),
+                notificationType == NotificationType.CANCELED ? "취소" : "실패");
+
+        String message = String.format("결제가 %s되었습니다.",
+                notificationType == NotificationType.CANCELED ? "취소" : "실패");
 
         // 이메일 알림 생성
         boolean emailExists = alarmRepository.existsAlarm(reservationId, AlarmTargetType.PAYMENT, notificationType, AlarmChannel.EMAIL);
