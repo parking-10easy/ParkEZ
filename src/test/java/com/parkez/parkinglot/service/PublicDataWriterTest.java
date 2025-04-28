c99ec178c98c0a7641396d25466bbe22d53b6cbc
Seoyeon <dev_seoyeon@mythologyteams.com>
2025-04-28 17:52:47 +0900

    test(parking-lot): 테스트용 DB 스키마 추가 #110

--- src/test/java/com/parkez/parkinglot/service/PublicDataWriterTest.java
+++ src/test/java/com/parkez/parkinglot/service/PublicDataWriterTest.java
@@ -24,7 +24,6 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@ExtendWith(SpringExtension.class)
 @SpringBootTest
 @ActiveProfiles("test")
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
