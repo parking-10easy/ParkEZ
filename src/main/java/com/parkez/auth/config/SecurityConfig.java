8b58087e51abdd87e882a338f52fb43bd02ae9a0
jun5-soodal <j01024749503@gmail.com>
2025-04-28 12:14:13 +0900

    chore: 프로메테우스 설정 추가 #122

--- src/main/java/com/parkez/auth/config/SecurityConfig.java
+++ src/main/java/com/parkez/auth/config/SecurityConfig.java
@@ -11,12 +11,10 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.servletapi.SecurityContextHolderAwareRequestFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-import org.springframework.security.web.util.matcher.RegexRequestMatcher;
 
 import com.parkez.auth.authentication.filter.ExceptionHandlerFilter;
 import com.parkez.auth.authentication.filter.JwtAuthenticationFilter;
 import com.parkez.auth.authentication.handler.CustomAuthenticationEntryPoint;
-import com.parkez.user.domain.enums.UserRole;
 
 import lombok.RequiredArgsConstructor;
 
@@ -26,45 +24,45 @@ import lombok.RequiredArgsConstructor;
 @EnableMethodSecurity(securedEnabled = true)
 public class SecurityConfig {
 
-    private final JwtAuthenticationFilter jwtAuthenticationFilter;
-    private final ExceptionHandlerFilter exceptionHandlerFilter;
-    private final CustomAuthenticationEntryPoint customAuthenticationEntryPoint;
+	private final JwtAuthenticationFilter jwtAuthenticationFilter;
+	private final ExceptionHandlerFilter exceptionHandlerFilter;
+	private final CustomAuthenticationEntryPoint customAuthenticationEntryPoint;
 
-    @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        String[] SWAGGER_URI = {
-                "/v3/api-docs/**",
-                "/swagger-ui/**",
-                "/swagger-ui.index.html",
-        };
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		String[] SWAGGER_URI = {
+			"/v3/api-docs/**",
+			"/swagger-ui/**",
+			"/swagger-ui.index.html",
+		};
 
-        return http
-                .csrf(AbstractHttpConfigurer::disable)
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .exceptionHandling(exceptionHandling -> exceptionHandling
-                        .authenticationEntryPoint(customAuthenticationEntryPoint))
-                .addFilterBefore(exceptionHandlerFilter, SecurityContextHolderAwareRequestFilter.class)
-                .addFilterBefore(jwtAuthenticationFilter, SecurityContextHolderAwareRequestFilter.class)
-                .formLogin(AbstractHttpConfigurer::disable)
-                .anonymous(AbstractHttpConfigurer::disable)
-                .httpBasic(AbstractHttpConfigurer::disable)
-                .logout(AbstractHttpConfigurer::disable)
-                .rememberMe(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(
-                        auth -> auth
-                                .requestMatchers(new AntPathRequestMatcher("/api/*/auth/signup/social/**")).authenticated()
-                                .requestMatchers(new AntPathRequestMatcher("/api/*/auth/**")).permitAll()
-                                .requestMatchers("/actuator/health").permitAll()
-                                .requestMatchers(SWAGGER_URI).permitAll()
-                                .requestMatchers("/toss-test/**").permitAll()
-                                .anyRequest().authenticated()
-                )
-                .build();
-    }
+		return http
+			.csrf(AbstractHttpConfigurer::disable)
+			.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+			.exceptionHandling(exceptionHandling -> exceptionHandling
+				.authenticationEntryPoint(customAuthenticationEntryPoint))
+			.addFilterBefore(exceptionHandlerFilter, SecurityContextHolderAwareRequestFilter.class)
+			.addFilterBefore(jwtAuthenticationFilter, SecurityContextHolderAwareRequestFilter.class)
+			.formLogin(AbstractHttpConfigurer::disable)
+			.anonymous(AbstractHttpConfigurer::disable)
+			.httpBasic(AbstractHttpConfigurer::disable)
+			.logout(AbstractHttpConfigurer::disable)
+			.rememberMe(AbstractHttpConfigurer::disable)
+			.authorizeHttpRequests(
+				auth -> auth
+					.requestMatchers("/actuator/health", "/actuator/prometheus").permitAll()
+					.requestMatchers(SWAGGER_URI).permitAll()
+					.requestMatchers(new AntPathRequestMatcher("/api/*/auth/**")).permitAll()
+					.requestMatchers(new AntPathRequestMatcher("/api/*/auth/signup/social/**")).authenticated()
+					.requestMatchers("/toss-test/**").permitAll()
+					.anyRequest().authenticated()
+			)
+			.build();
+	}
 
 }
